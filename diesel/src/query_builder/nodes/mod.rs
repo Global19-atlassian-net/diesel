a281d41aa54c4095a0cf5ffd3f09413b4512e524
Sean Griffin <sean@seantheprogrammer.com>
2017-04-30 10:55:08 -0400

    Refactor join construction to be much more composeable
    This moves most of the knowledge of join construction out of `JoinTo`
    and into the join methods themselves. Aside from exposing less of our
    internals, this is useful to us because it's much easier to implement
    `JoinTo` on joins themselves than it is to implement the specific join
    methods.
    
    The downside to this approach is that it gives us less specific control
    over certain types, and means that
    `users.inner_join(posts).inner_join(settings)` will likely have the type
    `(User, (Post, Setting))` instead of `(User, Post, Setting)` which will
    make it unfortunately difficult to differentiate
    `user.inner_join(posts).inner_join(comments)` from
    `user.inner_join(posts.inner_join(comments))`.
    
    Still, I think this is the path that will get us to *some* form of
    multi-table joins, and we can always refine later. This structure should
    make it fairly simple to add all of:
    
    - Joining to subselects
    - Cross joins
    - Join with explicit on-clauses
    - Self-referential joins (though not through the associations API)
    
    Additionally, the only real blocker remaining for multi-table joins is
    implementing `SelectableExpression` which should hopefully be unblocked
    by Rust soon.

--- diesel/src/query_builder/nodes/mod.rs
+++ diesel/src/query_builder/nodes/mod.rs
@@ -20,58 +20,6 @@ impl<'a, DB: Backend> QueryFragment<DB> for Identifier<'a> {
 }
 
 #[derive(Debug, Copy, Clone)]
-pub struct Join<T, U, V, W> {
-    lhs: T,
-    rhs: U,
-    predicate: V,
-    join_type: W,
-}
-
-impl<T, U, V, W> Join<T, U, V, W> {
-    pub fn new(lhs: T, rhs: U, predicate: V, join_type: W) -> Self {
-        Join {
-            lhs: lhs,
-            rhs: rhs,
-            predicate: predicate,
-            join_type: join_type,
-        }
-    }
-}
-
-impl<T, U, V, W, DB> QueryFragment<DB> for Join<T, U, V, W> where
-    DB: Backend,
-    T: QueryFragment<DB>,
-    U: QueryFragment<DB>,
-    V: QueryFragment<DB>,
-    W: QueryFragment<DB>,
-{
-    fn to_sql(&self, out: &mut DB::QueryBuilder) -> BuildQueryResult {
-        try!(self.lhs.to_sql(out));
-        try!(self.join_type.to_sql(out));
-        out.push_sql(" JOIN ");
-        try!(self.rhs.to_sql(out));
-        out.push_sql(" ON ");
-        try!(self.predicate.to_sql(out));
-        Ok(())
-    }
-
-    fn collect_binds(&self, out: &mut DB::BindCollector) -> QueryResult<()> {
-        try!(self.lhs.collect_binds(out));
-        try!(self.join_type.collect_binds(out));
-        try!(self.rhs.collect_binds(out));
-        try!(self.predicate.collect_binds(out));
-        Ok(())
-    }
-
-    fn is_safe_to_cache_prepared(&self) -> bool {
-        self.lhs.is_safe_to_cache_prepared() &&
-            self.join_type.is_safe_to_cache_prepared() &&
-            self.rhs.is_safe_to_cache_prepared() &&
-            self.predicate.is_safe_to_cache_prepared()
-    }
-}
-
-#[derive(Debug, Copy, Clone)]
 pub struct InfixNode<'a, T, U> {
     lhs: T,
     rhs: U,
